e35bc0595a2d12c18d6542870156fb1c08539b1e
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-06 23:01:03 +0900

    cospro1차 7번 동적할당, 포인터 char 배열

--- cospro1_7/cospro1_7.c
+++ cospro1_7/cospro1_7.c
@@ -7,10 +7,20 @@
 문제 : 중복문자 삭제하기
 알파벳 문자열이 주어질 때, 연속하는 중복 문자를 삭제하려고 한다.
 */
-int solution(char* characters) {
+char* solution(char* characters) { //문자열 매개변수로 받음
+    //중복하지 않은 문자를 담을 포인터 char 배열을 동적할당 생성 
     char* answer = (char*)malloc(sizeof(char) * 103);
+    int str_len = 0;
     answer[0] = characters[0];
-
+    str_len++;
+    for (int i = 1; i < strlen(characters); i++) {
+        if (characters[i - 1] != characters[i]) {
+            answer[str_len] = characters[i];
+            str_len++;
+        }
+    }
+    answer[str_len] = NULL;
+    return answer;
 }
 
 int main() {
