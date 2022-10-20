788ce8a0db2300ddd37c1d3892d1800c1c5d7df5
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-10-21 08:30:30 +0900

    정렬찾기

---
+++ FibonacciSearch/FibonacciSearch.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int fibonacciSearch(int a[], int n, int key) {
+	int index = 0, pos, k = 0;
+	static int fibo[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+	while (fibo[k] < n) k++;
+
+	while (k > 0) {
+		pos = index + fibo[--k];
+		if (pos >= n || key < a[pos]);
+		else if (key > a[pos]) {
+			index = pos;
+			k--;
+		}
+		else return pos;
+	}
+	return -1;
+}
+
+int main(void) {
+
+	int a[] = { 5,12,17,20,25,33,38,41,59,65,68,70,75,87,92,95,99,100,102,110,115,117,120,125,128 };
+	int i, n, key;
+
+	n = sizeof(a) / sizeof(int);
+
+	printf("검색 대상: ");
+	for (i = 0; i < n; i++) printf("%4d", a[i]);
+	printf("\n");
+
+	printf("찾고자 하는 값 입력:");
+	scanf_s("%d", &key);
+
+	i = fibonacciSearch(a, n, key);
+
+	if (i >= 0)
+		printf("\nfound %d at index %d\n", key, i);
+	else
+		printf("\nnot found\n");
+
+	return 0;
+}
